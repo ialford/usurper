4f0868b7031b1784cf712849642f48383a6d7c38
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-10-18 12:02:10 -0400

    Update styling.

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -7,55 +7,61 @@ import Image from 'components/Image'
 import Link from 'components/Interactive/Link'
 import Tags from 'components/Interactive/Tags'
 
-import styles from './style.module.css'
+import './style.css'
 
 const EventCard = ({ entry, isLast, showDescription, showImage, showTags, onTagClick }) => {
+  const linkAriaLabel = entry.title + ' on ' + entry.displayDate + ' at ' + entry.displayTime
+  const linkPath = '/event/' + entry.slug
   return (
-    <div className='event-card' itemScope itemType='http://schema.org/Event'>
-      <Link
-        ariaLabel={entry.title + ' on ' + entry.displayDate + ' at ' + entry.displayTime}
-        to={'/event/' + entry.slug}
-        itemProp='mainEntityOfPage'
-      >
+    <React.Fragment>
+      <div className='event-card' itemScope itemType='http://schema.org/Event' itemProp='mainEntityOfPage'>
         <meta itemProp='startDate' content={entry.startDate} />
         <meta itemProp='endDate' content={entry.endDate} />
         <div itemProp='location' itemScope itemType='http://schema.org/Place' hidden>
           <meta itemProp='address' content={entry.locationText} />
         </div>
-        { showImage && (
-          <Image cfImage={entry.representationalImage} className='card-image' itemProp='image' />
-        )}
-        <div className='date'>
-          {entry.displayDate}
+        <div>
+          { showImage && (
+            <Link ariaLabel={linkAriaLabel} to={linkPath} className='card-image'>
+              <Image cfImage={entry.representationalImage} itemProp='image' />
+            </Link>
+          )}
         </div>
-        { entry.displayTime && (
-          <div className='time'>
-            {entry.displayTime}
-          </div>
-        )}
-        <h2 itemProp='name'>{entry.title}</h2>
-        { entry.type && (
-          <div className={styles.eventType}>{entry.type}</div>
-        )}
+        <div>
+          <Link ariaLabel={linkAriaLabel} to={linkPath}>
+            <div className='date'>
+              {entry.displayDate}
+            </div>
+            { entry.displayTime && (
+              <div className='time'>
+                {entry.displayTime}
+              </div>
+            )}
+            <h2 itemProp='name'>{entry.title}</h2>
+            { entry.type && (
+              <div className='event-type'>{entry.type}</div>
+            )}
 
-        { showDescription && (
-          <div className='description' itemProp='description'>
-            <LibMarkdown>{entry.shortDescription}</LibMarkdown>
-          </div>
-        )}
-      </Link>
-      { showTags && (
-        <Tags
-          items={typy(entry.audience).safeArray.map(displayName => ({
-            key: displayName,
-            value: displayName,
-          }))}
-          onClick={(pair) => onTagClick('audience', [ pair.key ])}
-          ariaLabel='audience'
-        />
-      )}
-      { !isLast && <hr /> }
-    </div>
+            { showDescription && (
+              <div className='description' itemProp='description'>
+                <LibMarkdown>{entry.shortDescription}</LibMarkdown>
+              </div>
+            )}
+          </Link>
+          { showTags && (
+            <Tags
+              items={typy(entry.audience).safeArray.map(displayName => ({
+                key: displayName,
+                value: displayName,
+              }))}
+              onClick={(pair) => onTagClick('audience', [ pair.key ])}
+              ariaLabel='audience'
+            />
+          )}
+        </div>
+      </div>
+      { !isLast && <hr className='card-divider' /> }
+    </React.Fragment>
   )
 }
 
